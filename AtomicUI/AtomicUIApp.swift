d218b5b531d839a0c274c544afa1dc9145285217
Allan Canare <allan.canare@gmail.com>
2022-08-06 16:55:54 +0900

    Initial project commit

---
+++ AtomicUI/AtomicUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicUIApp.swift
+//  AtomicUI
+//
+//  Created by Allan Canare on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct AtomicUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
