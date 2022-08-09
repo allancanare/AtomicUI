4a7ab547631b206e259bc8d9c9b4eed4f1cde378
Allan Canare <allan.canare@gmail.com>
2022-08-09 21:58:27 +0900

    Added more styles to TextView

--- AtomicUI/DesignSystem/Atoms/TextView.swift
+++ AtomicUI/DesignSystem/Atoms/TextView.swift
@@ -36,98 +36,108 @@ extension AtomicUI.TextView {
         let font: AtomicUI.Font
         let color: AtomicUI.Color
         
-        // MARK: Heading - Medium - Dark Gray
+        // Heading - Medium - Dark Gray
         static let headingMediumDarkGrayDarkest = Style(font: .headingMedium,
                                                         color: .darkGrayDarkest)
         
-        // MARK: Heading - Small - Dark Gray
+        // Heading - Small - Dark Gray
         static let headingSmallDarkGrayDarkest = Style(font: .headingSmall,
                                                        color: .darkGrayDarkest)
         
-        // MARK: Heading - Extra Small - Dark Gray
-        static let headingExtraSmallDarkGrayDarkest = Style(font: .headingExtraSmall,
-                                                            color: .darkGrayDarkest)
+        // Heading - Extra Small - Highlight
+        static let headingExtraSmallHighlightLight = Style(font: .headingExtraSmall,
+                                                           color: .highlightLight)
         
-        static let headingExtraSmallDarkGrayDark = Style(font: .headingExtraSmall,
-                                                         color: .darkGrayDark)
-        
-        static let headingExtraSmallDarkGrayLight = Style(font: .headingExtraSmall,
-                                                          color: .darkGrayLight)
-        
-        static let headingExtraSmallDarkGrayLightest = Style(font: .headingExtraSmall,
-                                                             color: .darkGrayLightest)
-        
-        // MARK: Heading - Extra Small - Light Gray
+        // Heading - Extra Small - Light Gray
         static let headingExtraSmallLightGrayLightest = Style(font: .headingExtraSmall,
                                                               color: .lightGrayLightest)
         
-        // MARK: Body - Medium - Dark Gray
+        // Heading - Extra Small - Dark Gray
+        static let headingExtraSmallDarkGrayDarkest = Style(font: .headingExtraSmall,
+                                                            color: .darkGrayDarkest)
+        static let headingExtraSmallDarkGrayDark = Style(font: .headingExtraSmall,
+                                                         color: .darkGrayDark)
+        static let headingExtraSmallDarkGrayMedium = Style(font: .headingExtraSmall,
+                                                           color: .darkGrayMedium)
+        static let headingExtraSmallDarkGrayLight = Style(font: .headingExtraSmall,
+                                                          color: .darkGrayLight)
+        static let headingExtraSmallDarkGrayLightest = Style(font: .headingExtraSmall,
+                                                             color: .darkGrayLightest)
+        
+        // Body - Large - Light Gray
+        static let bodyLargeLightGrayLightest = Style(font: .bodyLarge,
+                                                      color: .lightGrayLightest)
+        
+        // Body - Large - Dark Gray
+        static let bodyLargeDarkGrayMedium = Style(font: .bodyLarge,
+                                                   color: .darkGrayMedium)
+        
+        // Body - Medium - Dark Gray
         static let bodyMediumDarkGrayDarkest = Style(font: .bodyMedium,
                                                      color: .darkGrayDarkest)
-        
         static let bodyMediumDarkGrayLight = Style(font: .bodyMedium,
                                                    color: .darkGrayLight)
-        
         static let bodyMediumDarkGrayLightest = Style(font: .bodyMedium,
                                                       color: .darkGrayLightest)
         
-        // MARK: Body - Medium - Error
+        // Body - Medium - Error
         static let bodyMediumErrorDark = Style(font: .bodyMedium,
                                                color: .errorDark)
         
-        // MARK: Body - Small - Dark Gray
+        // Body - Small - Dark Gray
         static let bodySmallDarkGrayDarkest = Style(font: .bodySmall,
                                                     color: .darkGrayDarkest)
-        
         static let bodySmallDarkGrayMedium = Style(font: .bodySmall,
                                                    color: .darkGrayMedium)
-        
         static let bodySmallDarkGrayLight = Style(font: .bodySmall,
                                                   color: .darkGrayLight)
         
-        // MARK: Body - Extra Small - Dark Gray
-        static let bodyExtraSmallDarkGrayLight = Style(font: .bodyExtraSmall,
-                                                       color: .darkGrayLight)
-        
-        static let bodyExtraSmallDarkGrayLightest = Style(font: .bodyExtraSmall,
-                                                          color: .darkGrayLightest)
-        
-        // MARK: Body - Extra Small - Light Gray
+        // Body - Extra Small - Light Gray
         static let bodyExtraSmallLightGrayDark = Style(font: .bodyExtraSmall,
                                                        color: .lightGrayDark)
         
-        // MARK: Body - Extra Small - Error
+        // Body - Extra Small - Dark Gray
+        static let bodyExtraSmallDarkGrayLight = Style(font: .bodyExtraSmall,
+                                                       color: .darkGrayLight)
+        static let bodyExtraSmallDarkGrayLightest = Style(font: .bodyExtraSmall,
+                                                          color: .darkGrayLightest)
+        
+        // Body - Extra Small - Error
         static let bodyExtraSmallErrorDark = Style(font: .bodyExtraSmall,
                                                    color: .errorDark)
         
-        // MARK: Action - Medium - Dark Gray
-        static let actionMediumDarkGrayLight = Style(font: .actionMedium,
-                                                     color: .darkGrayLight)
-        
-        // MARK: Action - Medium - Highlight
+        // Action - Medium - Highlight
         static let actionMediumHighlightDarkest = Style(font: .actionMedium,
                                                         color: .highlightDarkest)
         
-        // MARK: Action - Medium - Light Gray
+        // Action - Medium - Light Gray
         static let actionMediumLightGrayLightest = Style(font: .actionMedium,
                                                          color: .lightGrayLightest)
         
-        // MARK: Action - Small - Dark Gray
+        // Action - Medium - Dark Gray
+        static let actionMediumDarkGrayLight = Style(font: .actionMedium,
+                                                     color: .darkGrayLight)
+        
+        // Action - Small - Dark Gray
         static let actionSmallDarkGrayDarkest = Style(font: .actionSmall,
                                                       color: .darkGrayDarkest)
         
-        // MARK: Caption - Medium - Light Gray
+        // Caption - Medium - Highlight
+        static let captionMediumHighlightDarkest = Style(font: .captionMedium,
+                                                         color: .highlightDarkest)
+        static let captionMediumHighlightLight = Style(font: .captionMedium,
+                                                       color: .highlightLight)
+        
+        // Caption - Medium - Light Gray
         static let captionMediumLightGrayLightest = Style(font: .captionMedium,
                                                           color: .lightGrayLightest)
         
-        // MARK: Caption - Medium - Highlight
-        static let captionMediumHighlightestDarkest = Style(font: .captionMedium,
-                                                            color: .highlightDarkest)
+        // Caption - Medium - Dark Gray
+        static let captionMediumDarkGrayLightest = Style(font: .captionMedium,
+                                                         color: .darkGrayLightest)
         
         // Default
-        static let `default` = headingSmallDarkGrayDarkest
-        
-        // TODO: List Item
+        static let `default` = bodyMediumDarkGrayDarkest
     }
 }
 
@@ -148,15 +158,26 @@ struct TextView_Previews: PreviewProvider {
                 
                 Section("Heading - Extra Small") {
                     AtomicUI.TextView("Hello World!",
+                                      style: .headingExtraSmallHighlightLight)
+                    AtomicUI.TextView("Hello World!",
+                                      style: .headingExtraSmallLightGrayLightest)
+                    AtomicUI.TextView("Hello World!",
                                       style: .headingExtraSmallDarkGrayDarkest)
                     AtomicUI.TextView("Hello World!",
                                       style: .headingExtraSmallDarkGrayDark)
                     AtomicUI.TextView("Hello World!",
+                                      style: .headingExtraSmallDarkGrayMedium)
+                    AtomicUI.TextView("Hello World!",
                                       style: .headingExtraSmallDarkGrayLight)
                     AtomicUI.TextView("Hello World!",
                                       style: .headingExtraSmallDarkGrayLightest)
+                }
+                
+                Section("Body - Large") {
                     AtomicUI.TextView("Hello World!",
-                                      style: .headingExtraSmallLightGrayLightest)
+                                      style: .bodyLargeLightGrayLightest)
+                    AtomicUI.TextView("Hello World!",
+                                      style: .bodyLargeDarkGrayMedium)
                 }
                 
                 Section("Body - Medium") {
@@ -181,22 +202,22 @@ struct TextView_Previews: PreviewProvider {
                 
                 Section("Body - Extra Small") {
                     AtomicUI.TextView("Hello World!",
+                                      style: .bodyExtraSmallLightGrayDark)
+                    AtomicUI.TextView("Hello World!",
                                       style: .bodyExtraSmallDarkGrayLight)
                     AtomicUI.TextView("Hello World!",
                                       style: .bodyExtraSmallDarkGrayLightest)
-                    AtomicUI.TextView("Hello World!",
-                                      style: .bodyExtraSmallLightGrayDark)
                     AtomicUI.TextView("Hello World!",
                                       style: .bodyExtraSmallErrorDark)
                 }
                 
                 Section("Action - Medium") {
                     AtomicUI.TextView("Hello World!",
-                                      style: .actionMediumDarkGrayLight)
-                    AtomicUI.TextView("Hello World!",
                                       style: .actionMediumHighlightDarkest)
                     AtomicUI.TextView("Hello World!",
                                       style: .actionMediumLightGrayLightest)
+                    AtomicUI.TextView("Hello World!",
+                                      style: .actionMediumDarkGrayLight)
                 }
                 
                 Section("Action - Small") {
@@ -206,9 +227,13 @@ struct TextView_Previews: PreviewProvider {
                 
                 Section("Caption - Medium") {
                     AtomicUI.TextView("Hello World!",
+                                      style: .captionMediumHighlightDarkest)
+                    AtomicUI.TextView("Hello World!",
+                                      style: .captionMediumHighlightLight)
+                    AtomicUI.TextView("Hello World!",
                                       style: .captionMediumLightGrayLightest)
                     AtomicUI.TextView("Hello World!",
-                                      style: .captionMediumHighlightestDarkest)
+                                      style: .captionMediumDarkGrayLightest)
                 }
             }
             .navigationTitle("TextViews")
